2bec4f30d4d64954896a7e6563f62e128fb02df7
Yinka Ash <slankeyinc@gmail.com>
2017-03-27 22:48:32 +0800

    Add Validtaions to Contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,23 @@ class ContactsController < ApplicationController
 	end
 
 	def create
-		
+		@contact = Contact.new(contact_params)
+
+
+		if @contact.save
+			flash[:success] = "Message Sent Successfully"
+			redirect_to new_contact_path
+		else
+			flash[:danger] = "Message Failed! Please confirm the fields are correctly filled"
+			render 'new'
+		end
+	end
+
+
+	private
+
+	def contact_params
+		params.require(:contact).permit(:name, :email, :comments)		
 	end
 
 end
